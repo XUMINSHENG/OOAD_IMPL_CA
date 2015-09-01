06c475a48a5e3708a61b4ddb4a139471c565123e
XUMINSHENG <349768441@qq.com>
2015-09-01 21:12:04 +0800

    setup env and search Radio Program

--- PRMS/src/java/sg/edu/nus/iss/phoenix/radioprogram/controller/SearchRadioProgramCmd.java
+++ PRMS/src/java/sg/edu/nus/iss/phoenix/radioprogram/controller/SearchRadioProgramCmd.java
@@ -8,11 +8,14 @@ package sg.edu.nus.iss.phoenix.radioprogram.controller;
 import at.nocturne.api.Action;
 import at.nocturne.api.Perform;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import sg.edu.nus.iss.phoenix.radioprogram.delegate.ProgramDelegate;
 import sg.edu.nus.iss.phoenix.radioprogram.delegate.ReviewSelectProgramDelegate;
+import sg.edu.nus.iss.phoenix.radioprogram.entity.RPSearchObject;
 import sg.edu.nus.iss.phoenix.radioprogram.entity.RadioProgram;
 
 /**
@@ -23,9 +26,22 @@ import sg.edu.nus.iss.phoenix.radioprogram.entity.RadioProgram;
 public class SearchRadioProgramCmd implements Perform {
     @Override
     public String perform(String path, HttpServletRequest req, HttpServletResponse resp) throws IOException, ServletException {
-        //ReviewSelectProgramDelegate del = new ReviewSelectProgramDelegate();
-        //List<RadioProgram> data = del.reviewSelectRadioProgram();
-        //req.setAttribute("rps", data);
+        ProgramDelegate del = new ProgramDelegate();
+        RPSearchObject rpso = new RPSearchObject();
+        rpso.setName(req.getParameter("name"));
+        rpso.setDescription(req.getParameter("description"));
+        System.out.println(rpso.toString());
+        
+        ArrayList<RadioProgram> data = null;
+        
+        if (!rpso.getName().isEmpty() || !rpso.getDescription().isEmpty() )
+            data = del.findRPByCriteria(rpso);
+        else 
+            data = del.findAllRP();
+                
+        req.setAttribute("name", rpso.getName());
+        req.setAttribute("description", rpso.getDescription());
+        req.setAttribute("searchrplist", data);
         return "/pages/searchrp.jsp";
     }
 }
