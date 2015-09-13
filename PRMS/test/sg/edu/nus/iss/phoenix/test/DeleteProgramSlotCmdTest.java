81ebf0b059f2ce1ff17e6fd278ad8bad24dbe424
XUMINSHENG <349768441@qq.com>
2015-09-13 18:13:43 +0800

    Upd UnitTest for deleteProgramSlotCmd

--- PRMS/test/sg/edu/nus/iss/phoenix/test/DeleteProgramSlotCmdTest.java
+++ PRMS/test/sg/edu/nus/iss/phoenix/test/DeleteProgramSlotCmdTest.java
@@ -6,8 +6,6 @@
 package sg.edu.nus.iss.phoenix.test;
 
 import com.mysql.jdbc.jdbc2.optional.MysqlDataSource;
-import java.sql.Connection;
-import java.sql.DriverManager;
 import java.util.ArrayList;
 import java.util.List;
 import javax.naming.Context;
@@ -16,7 +14,6 @@ import javax.naming.NamingException;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.sql.DataSource;
 import org.junit.AfterClass;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.fail;
@@ -28,6 +25,11 @@ import sg.edu.nus.iss.phoenix.schedule.controller.DeleteProgramSlotCmd;
 import sg.edu.nus.iss.phoenix.schedule.entity.ProgramSlot;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javax.servlet.http.HttpSession;
+import org.junit.Before;
+import sg.edu.nus.iss.phoenix.authenticate.entity.Role;
+import sg.edu.nus.iss.phoenix.authenticate.entity.User;
+import sg.edu.nus.iss.phoenix.core.exceptions.NotFoundException;
 import sg.edu.nus.iss.phoenix.radioprogram.entity.RadioProgram;
 import sg.edu.nus.iss.phoenix.schedule.dao.impl.ScheduleDAOImpl;
 import sg.edu.nus.iss.phoenix.user.entity.Presenter;
@@ -39,10 +41,16 @@ import sg.edu.nus.iss.phoenix.util.Util;
  */
 public class DeleteProgramSlotCmdTest {
     
-    //private static Connection connection;
+    // Action Class is stateless
+    private DeleteProgramSlotCmd deleteProgramSlotCmd = new DeleteProgramSlotCmd();
+    private User user;
+    private HttpSession session;
+    private HttpServletRequest req;
+    private HttpServletResponse resp;
 
     @BeforeClass
     public static void before() {
+        
         // rcarver - setup the jndi context and the datasource
         try {
             // Create initial context
@@ -70,36 +78,49 @@ public class DeleteProgramSlotCmdTest {
     public static void after() {
 
     }
+    
+    @Before
+    public void Setup(){
+        user = mock(User.class);
+        session = mock(HttpSession.class);
+        req = mock(HttpServletRequest.class);
+        resp = mock(HttpServletResponse.class);
+    }
 
     @Test
-    public void deleteProgramSlot() throws Exception {
+    public void performSuccTest() throws Exception {
 
-        DeleteProgramSlotCmd deleteProgramSlotCmd = new DeleteProgramSlotCmd();
+        
+        when(user.hasRole("manager"))
+                .thenReturn(true);
 
-        RequestDispatcher rd = mock(RequestDispatcher.class);                
-
-        HttpServletRequest req = mock(HttpServletRequest.class);
+        
+        when(session.getAttribute("user"))
+                .thenReturn(user);
+        
+        
+        when(req.getSession())
+                .thenReturn(session);
         when(req.getParameter("dateOfProgram"))
                 .thenReturn("2015-09-15");
         when(req.getParameter("startTime"))
                 .thenReturn("07:30:00");
-        
-        when(req.getRequestDispatcher("/pages/crudsc.jsp"))
-                .thenReturn(rd);
 
-        HttpServletResponse resp = mock(HttpServletResponse.class);
-
+        // perform test
+        String forwardPath;
         try {
-            deleteProgramSlotCmd.perform(null, req, resp);
-            
+            forwardPath = deleteProgramSlotCmd.perform(null, req, resp);
         } catch (Exception ex) {
             fail("When calling perform");
             return;
         }
-
+        
+        // verify test
+        verify(req).getSession();
+        verify(session).getAttribute("user");
+        verify(user).hasRole("manager");
         verify(req).getParameter("dateOfProgram");
         verify(req).getParameter("startTime");
-        //verify(rd).forward(req, resp);
 
         ArgumentCaptor<String> stringCaptor = ArgumentCaptor.forClass(String.class);
         ArgumentCaptor<List> listCaptor = ArgumentCaptor.forClass(List.class);
@@ -131,10 +152,325 @@ public class DeleteProgramSlotCmdTest {
  
         // add to list
         expected.add(expProgramSlot);
-        
+        assertEquals("forwardPath", "/pages/crudsc.jsp", forwardPath);
         assertEquals("ProgramSlotsData", "pss", stringCaptor.getValue());
         assertEquals("Remaining Slot" , expected, listCaptor.getValue());
         
     }
+    
+    @Test
+    public void performNotLogInTest() throws Exception {
+        
+        when(user.hasRole("manager"))
+                .thenReturn(true);
+        
+        // user do not login
+        when(session.getAttribute("user"))
+                .thenReturn(null);
 
+        when(req.getSession())
+                .thenReturn(session);
+        when(req.getParameter("dateOfProgram"))
+                .thenReturn("2015-09-15");
+        when(req.getParameter("startTime"))
+                .thenReturn("07:30:00");
+
+        // perform test
+        String forwardPath;
+        try {
+            forwardPath = deleteProgramSlotCmd.perform(null, req, resp);
+        } catch (Exception ex) {
+            fail("When calling perform");
+            return;
+        }
+        
+        // verify test
+        verify(req).getSession();
+        verify(session).getAttribute("user");
+
+        ArgumentCaptor<String> nameCaptor = ArgumentCaptor.forClass(String.class);
+        ArgumentCaptor<String> valueCaptor = ArgumentCaptor.forClass(String.class);
+
+        verify(req).setAttribute(nameCaptor.capture(), valueCaptor.capture());
+        
+        assertEquals("forwardPath", "/pages/error.jsp", forwardPath);
+        assertEquals("varName", "errorMsg", nameCaptor.getValue());
+        assertEquals("errorMsg" , 
+                "You do not have the privileges to perform this operation", 
+                valueCaptor.getValue());
+    }
+    
+    @Test
+    public void performNoPrivilegeTest() throws Exception {
+        // user do not have to role
+        when(user.hasRole("manager"))
+                .thenReturn(false);
+        
+        when(session.getAttribute("user"))
+                .thenReturn(user);
+
+        
+        when(req.getSession())
+                .thenReturn(session);
+        when(req.getParameter("dateOfProgram"))
+                .thenReturn("2015-09-15");
+        when(req.getParameter("startTime"))
+                .thenReturn("07:30:00");
+
+        // perform test
+        String forwardPath;
+        try {
+            forwardPath = deleteProgramSlotCmd.perform(null, req, resp);
+        } catch (Exception ex) {
+            fail("When calling perform");
+            return;
+        }
+        
+        // verify test
+        verify(req).getSession();
+        verify(session).getAttribute("user");
+        verify(user).hasRole("manager");
+
+        ArgumentCaptor<String> nameCaptor = ArgumentCaptor.forClass(String.class);
+        ArgumentCaptor<String> valueCaptor = ArgumentCaptor.forClass(String.class);
+
+        verify(req).setAttribute(nameCaptor.capture(), valueCaptor.capture());
+        
+        assertEquals("forwardPath", "/pages/error.jsp", forwardPath);
+        assertEquals("varName", "errorMsg", nameCaptor.getValue());
+        assertEquals("errorMsg" , 
+                "You do not have the privileges to perform this operation", 
+                valueCaptor.getValue());
+    }
+
+    @Test
+    public void performEmptyDateTest() throws Exception {
+        
+        when(user.hasRole("manager"))
+                .thenReturn(true);
+        
+        when(session.getAttribute("user"))
+                .thenReturn(user);
+
+        when(req.getSession())
+                .thenReturn(session);
+
+        // empty date
+        when(req.getParameter("dateOfProgram"))
+                .thenReturn("");
+        when(req.getParameter("startTime"))
+                .thenReturn("07:30:00");
+
+        // perform test
+        String forwardPath;
+        try {
+            forwardPath = deleteProgramSlotCmd.perform(null, req, resp);
+        } catch (Exception ex) {
+            fail("When calling perform");
+            return;
+        }
+        
+        // verify test
+        verify(req).getSession();
+        verify(session).getAttribute("user");
+        verify(user).hasRole("manager");
+        verify(req).getParameter("dateOfProgram");
+
+        ArgumentCaptor<String> nameCaptor = ArgumentCaptor.forClass(String.class);
+        ArgumentCaptor<String> valueCaptor = ArgumentCaptor.forClass(String.class);
+
+        verify(req).setAttribute(nameCaptor.capture(), valueCaptor.capture());
+        
+        assertEquals("forwardPath", "/pages/error.jsp", forwardPath);
+        assertEquals("varName", "errorMsg", nameCaptor.getValue());
+        assertEquals("errorMsg" , 
+                "Invalid input", 
+                valueCaptor.getValue());
+    }
+    
+    @Test
+    public void performInvalidDateTest() throws Exception {
+        
+        when(user.hasRole("manager"))
+                .thenReturn(true);
+        
+        when(session.getAttribute("user"))
+                .thenReturn(user);
+
+        when(req.getSession())
+                .thenReturn(session);
+
+        // incorrect date
+        when(req.getParameter("dateOfProgram"))
+                .thenReturn("a-02-30");
+        when(req.getParameter("startTime"))
+                .thenReturn("07:30:00");
+
+        // perform test
+        String forwardPath;
+        try {
+            forwardPath = deleteProgramSlotCmd.perform(null, req, resp);
+        } catch (Exception ex) {
+            fail("When calling perform");
+            return;
+        }
+        
+        // verify test
+        verify(req).getSession();
+        verify(session).getAttribute("user");
+        verify(user).hasRole("manager");
+        verify(req).getParameter("dateOfProgram");
+
+        ArgumentCaptor<String> nameCaptor = ArgumentCaptor.forClass(String.class);
+        ArgumentCaptor<String> valueCaptor = ArgumentCaptor.forClass(String.class);
+
+        verify(req).setAttribute(nameCaptor.capture(), valueCaptor.capture());
+        
+        assertEquals("forwardPath", "/pages/error.jsp", forwardPath);
+        assertEquals("varName", "errorMsg", nameCaptor.getValue());
+        assertEquals("errorMsg" , 
+                "Invalid input", 
+                valueCaptor.getValue());
+    }
+    
+    @Test
+    public void performEmptyTimeTest() throws Exception {
+        
+        when(user.hasRole("manager"))
+                .thenReturn(true);
+        
+        when(session.getAttribute("user"))
+                .thenReturn(user);
+
+        when(req.getSession())
+                .thenReturn(session);
+
+        // empty time
+        when(req.getParameter("dateOfProgram"))
+                .thenReturn("2015-09-15");
+        when(req.getParameter("startTime"))
+                .thenReturn("");
+
+        // perform test
+        String forwardPath;
+        try {
+            forwardPath = deleteProgramSlotCmd.perform(null, req, resp);
+        } catch (Exception ex) {
+            fail("When calling perform");
+            return;
+        }
+        
+        // verify test
+        verify(req).getSession();
+        verify(session).getAttribute("user");
+        verify(user).hasRole("manager");
+        verify(req).getParameter("dateOfProgram");
+        verify(req).getParameter("startTime");
+
+        ArgumentCaptor<String> nameCaptor = ArgumentCaptor.forClass(String.class);
+        ArgumentCaptor<String> valueCaptor = ArgumentCaptor.forClass(String.class);
+
+        verify(req).setAttribute(nameCaptor.capture(), valueCaptor.capture());
+        
+        assertEquals("forwardPath", "/pages/error.jsp", forwardPath);
+        assertEquals("varName", "errorMsg", nameCaptor.getValue());
+        assertEquals("errorMsg" , 
+                "Invalid input", 
+                valueCaptor.getValue());
+    }
+    
+    @Test
+    public void performInvalidTimeTest() throws Exception {
+        
+        when(user.hasRole("manager"))
+                .thenReturn(true);
+        
+        when(session.getAttribute("user"))
+                .thenReturn(user);
+
+        when(req.getSession())
+                .thenReturn(session);
+
+        // incorrect time
+        when(req.getParameter("dateOfProgram"))
+                .thenReturn("2015-02-30");
+        when(req.getParameter("startTime"))
+                .thenReturn("a7:30:00");
+
+        // perform test
+        String forwardPath;
+        try {
+            forwardPath = deleteProgramSlotCmd.perform(null, req, resp);
+        } catch (Exception ex) {
+            fail("When calling perform");
+            return;
+        }
+        
+        // verify test
+        verify(req).getSession();
+        verify(session).getAttribute("user");
+        verify(user).hasRole("manager");
+        verify(req).getParameter("dateOfProgram");
+        verify(req).getParameter("startTime");
+
+        ArgumentCaptor<String> nameCaptor = ArgumentCaptor.forClass(String.class);
+        ArgumentCaptor<String> valueCaptor = ArgumentCaptor.forClass(String.class);
+
+        verify(req).setAttribute(nameCaptor.capture(), valueCaptor.capture());
+        
+        assertEquals("forwardPath", "/pages/error.jsp", forwardPath);
+        assertEquals("varName", "errorMsg", nameCaptor.getValue());
+        assertEquals("errorMsg" , 
+                "Invalid input", 
+                valueCaptor.getValue());
+    }
+    
+    @Test
+    public void performDataNotFoundTest() throws Exception {
+        
+        when(user.hasRole("manager"))
+                .thenReturn(true);
+        
+        when(session.getAttribute("user"))
+                .thenReturn(user);
+
+        when(req.getSession())
+                .thenReturn(session);
+
+        // no such data
+        when(req.getParameter("dateOfProgram"))
+                .thenReturn("2015-02-01");
+        when(req.getParameter("startTime"))
+                .thenReturn("07:30:00");
+
+        // perform test
+        String forwardPath;
+        try {
+            forwardPath = deleteProgramSlotCmd.perform(null, req, resp);
+        } catch (Exception ex) {
+            fail("When calling perform");
+            return;
+        }
+        
+        // verify test
+        verify(req).getSession();
+        verify(session).getAttribute("user");
+        verify(user).hasRole("manager");
+        verify(req).getParameter("dateOfProgram");
+        verify(req).getParameter("startTime");
+
+        ArgumentCaptor<String> nameCaptor = ArgumentCaptor.forClass(String.class);
+        ArgumentCaptor<String> valueCaptor = ArgumentCaptor.forClass(String.class);
+
+        verify(req).setAttribute(nameCaptor.capture(), valueCaptor.capture());
+        
+        assertEquals("forwardPath", "/pages/error.jsp", forwardPath);
+        assertEquals("varName", "errorMsg", nameCaptor.getValue());
+        assertEquals("errorMsg" , 
+                new NotFoundException(
+                        "Object could not be deleted! (PrimaryKey not found)").
+                        getMessage(), 
+                valueCaptor.getValue());
+    }
+    
 }
