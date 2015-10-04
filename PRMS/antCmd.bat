ca30c2d10397ac63a2cc092b09e73b393364d814
charan <a0134630@u.nus.edu>
2015-10-04 23:54:36 +0800

    fix ant command

--- PRMS/antCmd.bat
+++ PRMS/antCmd.bat
@@ -3,4 +3,4 @@ CALL setenv.bat
 SET CUR_DIR=%CD%
 CALL stopGlassFish.bat
 CD %CUR_DIR%
-ant -f %CUR_DIR% -Dnb.internal.action.name=build -DforceRedeploy=false -Dbrowser.context=%CUR_DIR% clean dist
+ant -f %CUR_DIR% -Dnb.internal.action.name=build -DforceRedeploy=false -Dbrowser.context=%CUR_DIR% dist
